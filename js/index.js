60e6045bda1a53913363b5c65c96338bd91bad9c
Aaron <aaronmcavinue@gmail.com>
2016-12-29 00:56:45 +0000

    Add hello-world to index and unit test.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 require('babel-polyfill');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(
+        <h1>Hello World</h1>,
+        document.getElementById('app')
+    );
+});
---
+++ test/test-index.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
+
+describe('Index.js', function() {
+    it('Renders the app',  function() {
+        
+    });
+});
