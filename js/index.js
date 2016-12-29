dff9008a89479879789eac72b12f52575b9bff2a
Aaron <aaronmcavinue@gmail.com>
2016-12-29 23:57:39 +0000

    Test

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ require('babel-polyfill');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+// Test.
+
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(
         <h1>Hello World</h1>,
