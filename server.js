cbb84482bb92700b180c426994545b62c2e5eeff
Aaron <aaronmcavinue@gmail.com>
2016-12-31 01:43:00 +0000

    Add logic for converting from weather api. Update data model.

--- server.js
+++ server.js
@@ -12,7 +12,22 @@ app.use(bodyParser.json()); // Used for getting parameters in post requests.
  * Initial weather data structure.
  */
 var weatherData = {
-    
+    currently: {
+        summary: null,
+        precipProbability: null,
+        temperature: null,
+        feelsLike: null
+    },
+    forecast: {
+        hourly: {
+            summary: null,
+            hours: []
+        },
+        daily: {
+            summary: null,
+            days: []
+        }
+    }
 }
 
 /**
@@ -23,7 +38,7 @@ var weatherData = {
         if (err) {
             return res.status(400).json(err);
         }
-        convertData(data);
+        convertData(JSON.parse(data));
         return res.status(200).json(weatherData);
     });
 });
@@ -46,7 +61,98 @@ Converts the data from whatever api into the data model
 above, so the front end always recieves the same data.
 */
 function convertData(data) {
-    weatherData = data;
+    weatherData = {
+        currently: {
+            summary: data.currently.summary,
+            precipProbability: data.currently.precipProbability,
+            temperature: data.currently.temperature,
+            feelsLike: data.currently.apparentTemperature
+        },
+        forecast: {
+            hourly: {
+                summary: data.hourly.summary,
+                hours: [
+                    {
+                        summary: data.hourly.data[0].summary,
+                        precipProbability: data.hourly.data[0].precipProbability,
+                        temperature: data.hourly.data[0].temperature,
+                        feelsLike: data.hourly.data[0].apparentTemperature
+                    },
+                    {
+                        summary: data.hourly.data[1].summary,
+                        precipProbability: data.hourly.data[1].precipProbability,
+                        temperature: data.hourly.data[1].temperature,
+                        feelsLike: data.hourly.data[1].apparentTemperature
+                    },
+                    {
+                        summary: data.hourly.data[2].summary,
+                        precipProbability: data.hourly.data[2].precipProbability,
+                        temperature: data.hourly.data[2].temperature,
+                        feelsLike: data.hourly.data[2].apparentTemperature
+                    },
+                    {
+                        summary: data.hourly.data[3].summary,
+                        precipProbability: data.hourly.data[3].precipProbability,
+                        temperature: data.hourly.data[3].temperature,
+                        feelsLike: data.hourly.data[3].apparentTemperature
+                    },
+                    {
+                        summary: data.hourly.data[4].summary,
+                        precipProbability: data.hourly.data[4].precipProbability,
+                        temperature: data.hourly.data[4].temperature,
+                        feelsLike: data.hourly.data[4].apparentTemperature
+                    }
+                ]
+            },
+            daily: {
+                summary: data.daily.summary,
+                days: [
+                    {
+                        summary: data.daily.data[0].summary,
+                        precipProbability: data.daily.data[0].precipProbability,
+                        temperature: data.daily.data[0].temperature,
+                        feelsLike: data.daily.data[0].apparentTemperature
+                    },
+                    {
+                        summary: data.daily.data[1].summary,
+                        precipProbability: data.daily.data[1].precipProbability,
+                        temperature: data.daily.data[1].temperature,
+                        feelsLike: data.daily.data[1].apparentTemperature
+                    },
+                    {
+                        summary: data.daily.data[2].summary,
+                        precipProbability: data.daily.data[2].precipProbability,
+                        temperature: data.daily.data[2].temperature,
+                        feelsLike: data.daily.data[2].apparentTemperature
+                    },
+                    {
+                        summary: data.daily.data[3].summary,
+                        precipProbability: data.daily.data[3].precipProbability,
+                        temperature: data.daily.data[3].temperature,
+                        feelsLike: data.daily.data[3].apparentTemperature
+                    },
+                    {
+                        summary: data.daily.data[4].summary,
+                        precipProbability: data.daily.data[4].precipProbability,
+                        temperature: data.daily.data[4].temperature,
+                        feelsLike: data.daily.data[4].apparentTemperature
+                    },
+                    {
+                        summary: data.daily.data[5].summary,
+                        precipProbability: data.daily.data[5].precipProbability,
+                        temperature: data.daily.data[5].temperature,
+                        feelsLike: data.daily.data[5].apparentTemperature
+                    },
+                    {
+                        summary: data.daily.data[6].summary,
+                        precipProbability: data.daily.data[6].precipProbability,
+                        temperature: data.daily.data[6].temperature,
+                        feelsLike: data.daily.data[6].apparentTemperature
+                    }
+                ]
+            }
+        }
+    }
 }
  
 /**
