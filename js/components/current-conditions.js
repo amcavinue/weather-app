8204aeda9f7039694c88b89ae8550f5406f25348
Aaron <aaronmcavinue@gmail.com>
2017-01-02 00:52:01 +0000

    Add chart to component for hourly temps.

--- js/components/current-conditions.js
+++ js/components/current-conditions.js
@@ -1,8 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var LineChart = require("react-chartjs").Line;
 
 var CurrentConditions = function(props) {
-    console.log(props, 5);
+    var time = new Date();
+    var hours = time.getHours();
+    var mins = time.getMinutes();
+    
+    var hourlyTemp = {
+        labels: [ (hours + 1) + ':' + mins,  (hours + 2) + ':' + mins, (hours + 3) + ':' + mins, (hours + 4) + ':' + mins, (hours + 5) + ':' + mins],
+        datasets: [{
+            data: [props.weather.forecast.hourly.hours[0].temperature, props.weather.forecast.hourly.hours[1].temperature, props.weather.forecast.hourly.hours[2].temperature, props.weather.forecast.hourly.hours[3].temperature, props.weather.forecast.hourly.hours[4].temperature]
+        }]
+    };
+    
+    console.log(hourlyTemp, 17);
     
     return (
         <section>
@@ -11,6 +23,8 @@ var CurrentConditions = function(props) {
             <div>There is a { Math.round(props.weather.currently.precipProbability * 100) }% chance of precipitation.</div>
             <div>The temperature is { Math.round(props.weather.currently.temperature) }&deg;F&#47;{ Math.round((props.weather.currently.temperature - 32) * (5/9)) }&deg;C</div>
             <div>It feels like { Math.round(props.weather.currently.feelsLike) }&deg;F&#47;{ Math.round((props.weather.currently.feelsLike -32) * (5/9)) }&deg;C</div>
+            <h4>Hourly</h4>
+            <LineChart data={ hourlyTemp } />
         </section>
     );
 };
