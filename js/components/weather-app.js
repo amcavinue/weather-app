ac89a55f2c0683b25c646cf9d2cfa4dd32cb3c54
Aaron <aaronmcavinue@gmail.com>
2017-02-12 02:05:53 +0000

    Add cities interface element.

--- js/components/weather-app.js
+++ js/components/weather-app.js
@@ -12,6 +12,23 @@ const WeatherApp = React.createClass({
             this.props.dispatch(actions.resetError());
         }
     },
+    componentDidMount() {
+        $('#sidebar').w2sidebar({
+            name : 'sidebar',
+            flatButton: true,
+            nodes: [
+                { id: 'level-1', text: 'Cities', img: 'icon-folder', expanded: true, group: true, groupShowHide: false,
+                  nodes: [ { id: 'level-1-1', text: 'Los Angeles', icon: 'stateface stateface-ca' },
+                           { id: 'level-1-2', text: 'New York City', icon: 'stateface stateface-ny' },
+                           { id: 'level-1-3', text: 'Chicago', icon: 'stateface stateface-il' }
+                         ]
+                }
+            ],
+            onFlat: function (event) {
+                $('#sidebar').css('width', (event.goFlat ? '35px' : '200px'));
+            }
+        });
+    },
     render() {
         if (this.props.error) {
             alert('An error occurred while loading data for your area. Please reload the page or try again later.');
@@ -19,9 +36,13 @@ const WeatherApp = React.createClass({
         
         return (
             <div>
-                <h1>Weather for <span className="location">{this.props.coords.city + ', ' + this.props.coords.state}</span></h1>
-                <CurrentConditions weather={this.props.weather}/>
-                <ForecastDaily days={this.props.weather.forecast.days}/>
+                <div id="sidebar"></div>
+                
+                <div id="weather-app">
+                    <h1>Weather for <span className="location">{this.props.coords.city + ', ' + this.props.coords.state}</span></h1>
+                    <CurrentConditions weather={this.props.weather}/>
+                    <ForecastDaily days={this.props.weather.forecast.days}/>
+                </div>
             </div>
         );
     }
